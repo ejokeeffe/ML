3d643817af5d0499acdb6aa2acd1e892b84cc9ed
Eoin O'Keeffe <eokeeffe@gmail.com>
2015-08-20 11:50:50 +0100

    Documentation

--- ml_ext/examples.py
+++ ml_ext/examples.py
@@ -7,8 +7,12 @@ from ml_ext import pd
 from ml_ext import logging
 from ml_ext import random
 from ml_ext import numpy
+from ml_ext import lin_model
+from ml_ext import plt
+from ml_ext import sns
 
 random.seed(10)
+nupmy.random.seed(10)
 
 def sample_properties():
 	"""
@@ -41,3 +45,35 @@ def gen_simplemodel_data(n=1000,k=1):
 	df_x['y']=data
 
 	return (coefs,df_x)
+
+def run_simple_regression(n=1000,k=1,feature='X0'):
+	(coefs,df)=gen_simplemodel_data(n=n,k=k)
+	# logging.debug(df.head())
+	df.sort(feature,inplace=True)
+	lr=lin_model.LinModel()
+	X=df[df.columns[df.columns!='y']]
+	y=df.y
+
+
+	lr.fit(X=X,y=y)
+	lr.summary()
+	df_ci=lr.get_confidence_interval_for_mean(X)
+	df_pi=lr.get_prediction_interval(X)
+
+
+	(fig,ax)=plt.subplots(nrows=2,ncols=1,figsize=[14,12])
+
+	cols=sns.color_palette('husl',n_colors=4)
+	ax[0].scatter(X[feature],y,label='y',color=cols[3],alpha=0.4)
+
+	ax[0].plot(X[feature],df_pi['upper_pred'],label='pred',color=cols[1],alpha=0.5)
+	ax[0].plot(X[feature],df_pi['lower_pred'],color=cols[1],alpha=0.5)
+	ax[0].plot(X[feature],df_ci['upper_mean'],color=cols[2],alpha=0.5)
+	ax[0].plot(X[feature],df_ci['lower_mean'],label='mean_ci',color=cols[2],alpha=0.5)
+	ax[0].scatter(X[feature],df_pi['y_hat'],label='y_hat',color=cols[0],alpha=0.5)
+	ax[0].legend(loc='best')
+
+	ax[1].scatter(X[feature],y,label='y',color=cols[3],alpha=0.4)
+	ax[1].scatter(X[feature],df_ci['y_hat'],label='y_hat',color=cols[0],alpha=0.5)
+	ax[1].legend(loc='best')
+
---
+++ setup.py
@@ -0,0 +1,5 @@
+from distutils.core import setup
+
+setup(name='pyrate', 
+	version='0.1',
+	packages=['ml_ext'])
